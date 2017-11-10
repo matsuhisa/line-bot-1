882e1c4a6c987d3e868c03604f228a95c346e5d7
matsuhisa <matsuhisa+github@gmail.com>
2017-11-10 18:40:03 +0900

    WIP

--- app_main.rb
+++ app_main.rb
@@ -31,33 +31,88 @@ post '/callback' do
         #   text: event.message['text']
         # }
 
+        # message = {
+        #   type: "template",
+        #   altText: "this is a buttons template",
+        #   template: {
+        #       type: "buttons",
+        #       thumbnailImageUrl: "https://farm5.staticflickr.com/4244/35370997765_5f65dd60da_k_d.jpg",
+        #       title: "title",
+        #       text: "Please select ",
+        #       actions: [
+        #           {
+        #             type: "postback",
+        #             label: "Buy",
+        #             data: "action=buy&itemid=123"
+        #           },
+        #           {
+        #             type: "uri",
+        #             label: "電話する",
+        #             uri: "tel:09012345678"
+        #           },
+        #           {
+        #             type: "uri",
+        #             label: "詳細",
+        #             uri: "https://www.flickr.com/photos/matsuhisa/35370997765"
+        #           }
+        #       ]
+        #   }
+        # }
+
         message = {
-          type: "template",
-          altText: "this is a buttons template",
-          template: {
-              type: "buttons",
-              thumbnailImageUrl: "https://farm5.staticflickr.com/4244/35370997765_5f65dd60da_k_d.jpg",
-              title: "title",
-              text: "Please select ",
-              actions: [
+          "type": "template",
+          "altText": "this is a carousel template",
+          "template": {
+              "type": "carousel",
+              "columns": [
                   {
-                    type: "postback",
-                    label: "Buy",
-                    data: "action=buy&itemid=123"
+                    "thumbnailImageUrl": "https://farm5.staticflickr.com/4244/35370997765_5f65dd60da_k_d.jpg",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                      {
+                        type: "postback",
+                        label: "Buy",
+                        data: "action=buy&itemid=123"
+                      },
+                      {
+                        type: "uri",
+                        label: "電話する",
+                        uri: "tel:09012345678"
+                      },
+                      {
+                        type: "uri",
+                        label: "詳細",
+                        uri: "https://www.flickr.com/photos/matsuhisa/35370997765"
+                      }
+                    ]
                   },
                   {
-                    type: "uri",
-                    label: "電話する",
-                    uri: "tel:09012345678"
-                  },
-                  {
-                    type: "uri",
-                    label: "詳細",
-                    uri: "https://www.flickr.com/photos/matsuhisa/35370997765"
+                    "thumbnailImageUrl": "https://farm5.staticflickr.com/4288/35241101511_d919ed5d75_k_d.jpg",
+                    "title": "this is menu",
+                    "text": "description",
+                    "actions": [
+                      {
+                        type: "postback",
+                        label: "Buy",
+                        data: "action=buy&itemid=123"
+                      },
+                      {
+                        type: "uri",
+                        label: "電話する",
+                        uri: "tel:09012345678"
+                      },
+                      {
+                        type: "uri",
+                        label: "詳細",
+                        uri: "https://www.flickr.com/photos/matsuhisa/35241101511"
+                      }
+                    ]
                   }
               ]
           }
         }
+
         client.reply_message(event['replyToken'], message)
       when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
         response = client.get_message_content(event.message['id'])
